f646f37d1c2874a8510ff014ac251d7f23948385
Logan Cardoso <102470246+Logan-cic@users.noreply.github.com>
2022-11-16 10:55:58 -0300

    separando telas por nivel de dificuldade

---
+++ lib/Screens/Whatsapp/Exemplo_de_phishing_de_nivel_facil/Tela_de_email-1.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import '../../../Widgets/Email_Header/Sender_Card.dart';
+import '../../../Widgets/Email_Header/shipping_information.dart';
+
+
+class Screen16 extends StatelessWidget {
+  const Screen16({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 460,
+      child: MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.black,
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            elevation: 0,
+            title: Text(
+              "Game Phishing",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                //EmailTitle(label: "Últimas horas do Saldão do Cliente!"),
+                const SenderCard(label: "Amazon Inc."),
+                const ShippingInformation(
+                    sender: "Amazon Inc.",
+                    emailSender: "infosecurity@amazon.com.br",
+                    emailRecipient: "logancardoso4@gamil.com"),
+
+                SizedBox(height: 14),
+
+                Container(
+                  height: 602,
+                  width: 390,
+                  color: Colors.black,
+                  child: Column(
+                    children: [
+                      //Image.asset('images/amazon.jpg'),
+                      Row(
+                        children: [
+                          Text(
+                            "Dear Customer, ",
+                            textAlign: TextAlign.left,
+                            //textDirection: TextDirection.ltr,
+                            style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        "  We detected suspicious activity in your account and multiple password used for access your account.",
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                      SizedBox(height: 8),
+                      Text("We need you to confirm your account!",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 255, 183, 0),
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold)),
+                      Row(
+                        children: [
+                          Text("1. Click Here ",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 16)),
+                          Text(
+                            "to confirm your account.",
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text("2. Enter your informations. ",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 16)),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text("3. Finally your account will be confirmed. ",
+                              style:
+                              
+                                  TextStyle(color: Colors.white, fontSize: 16)),
+                        ],
+                      ),
+                      SizedBox(height: 6),
+                      Row(
+                        children: [
+                          Text("Note",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.red,
+                                  fontWeight: FontWeight.bold)),
+                          Text(
+                            ": If you dont't confirm it withing 48 hours, we will",
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            "close or suspend your account.",
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        children: [
+                          Text("Sincerely,",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 16)),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text("Amazon.",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold))
+                        ],
+                      ),
+                    ],
+                  ),
+                ),              
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
