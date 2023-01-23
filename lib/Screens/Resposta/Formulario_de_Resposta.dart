b3a396e4d76006d4d1fe7d192f480c528d84e6e7
Logan Cardoso <102470246+Logan-cic@users.noreply.github.com>
2023-01-23 00:13:39 -0300

    Ajustes na UI do Formulario de resposta

--- lib/Screens/Resposta/Formulario_de_Resposta.dart
+++ lib/Screens/Resposta/Formulario_de_Resposta.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -26,26 +28,37 @@ class _FormularioDeRespostaState extends State<FormularioDeResposta> {
   _salvandoRespostasDoUsuario() async {
     String simOuNao = _controllerSimOuNao.text;
     String motivo = _controllerMotivo.text;
-    String nome = _controllerNome.text;
+    // String nome = _controllerNome.text;
     Userdata user = Userdata();
     user.simOuNao = simOuNao;
     user.motivo = motivo;
-    user.name = nome;
+    // user.name = nome;
 
-    List<dynamic> resposta = [{"nome" : "asda" }, 2];
+    List<dynamic> resposta = [
+      {"nome": "asda"},
+      2
+    ];
+
+    // int i;
+    // final List<Map<dynamic, dynamic>> listDescricao = [];
+
+    // for (i = 1; i <= 2; i++) {
+    //   print("Enter name:");
+    //   String? name = stdin.readLineSync();
+    //   print("Enter OUTRO name:");
+    //   String? outroName = stdin.readLineSync();
+    //   listDescricao.add({"nome": name, "outro": outroName});
+    // }
+    // print(listDescricao);
 
     List<Map<String, dynamic>> locations = [
       {
-        'Nome': nome,
+        // 'Nome': nome,
         'city': 'Tokyo',
         'Latitude': 35.6762,
         'Longitude': 139.6503,
         'utcOffset': 9
-      }
-      ,
-
-      
-
+      },
     ];
 
     await Firebase.initializeApp();
@@ -53,7 +66,7 @@ class _FormularioDeRespostaState extends State<FormularioDeResposta> {
       // "Nome": resposta[0],
       // "sim ou não": resposta[1],
       // "motivo": user.motivo
-      "nome" : locations
+      "nome": locations
     });
 
     // .doc("001").set({
@@ -63,89 +76,114 @@ class _FormularioDeRespostaState extends State<FormularioDeResposta> {
     // });
   }
 
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Responda"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
+        title: Row(
           children: [
-            Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(bottom: 8),
-                  child: TextField(
-                    controller: _controllerSimOuNao,
-                    autofocus: true,
-                    keyboardType: TextInputType.text,
-                    style: TextStyle(fontSize: 17),
-                    decoration: InputDecoration(
-                        contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                        hintText: "O exemplo mostrado é Phishing? (sim ou não)",
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(32))),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(bottom: 8),
-                  child: TextField(
-                    controller: _controllerMotivo,
-                    autofocus: true,
-                    keyboardType: TextInputType.text,
-                    style: TextStyle(fontSize: 17),
-                    decoration: InputDecoration(
-                        contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                        hintText:
-                            "Como consguiu indetificar que o exemplo mostrado é phishing?",
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(32))),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(bottom: 8),
-                  child: TextField(
-                    controller: _controllerMotivo,
-                    autofocus: true,
-                    keyboardType: TextInputType.text,
-                    style: TextStyle(fontSize: 17),
-                    decoration: InputDecoration(
-                        contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                        hintText:
-                            "Caso não seja um phishing, nós diga o porque.",
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(32))),
-                  ),
-                ),
-                Padding(
-                    padding: EdgeInsets.only(top: 16, bottom: 10),
-                    child: TextButton(
-                      style: TextButton.styleFrom(
-                          foregroundColor: Colors.blue,
-                          padding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(32))),
-                      child: Text(
-                        "Responder",
-                        style: TextStyle(
-                            color: Color.fromARGB(255, 51, 5, 115),
-                            fontSize: 20),
-                      ),
-                      onPressed: (() {
-                        _salvandoRespostasDoUsuario();
-                      }),
-                    )),
-              ],
-            )
+            Text("Agora Responda"),
+            SizedBox(
+              width: 10,
+            ),
+            Icon(
+              Icons.format_align_right,
+            ),
           ],
+        ),
+        backgroundColor: Colors.red,
+      ),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 8, right: 8),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+               SizedBox(
+                height: 14,
+              ),
+                Text("O exemplo mostrado é Phishing?", style: TextStyle(fontSize: 20),),
+               SizedBox(
+                height: 12,
+              ),
+              TextFormField(
+                controller: _controllerSimOuNao,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  // hintText: 'O exemplo mostrado é Phishing? ',
+                  labelText: 'sim ou não',
+                ),
+                validator: (value) {
+                  if (value == null && (value != "sim"  || value != "não" )) {
+                    return 'Por favor responda apenas com sim ou não.';
+                  }
+                  return null;
+                },
+              ),
+               SizedBox(
+                height: 15,
+              ),
+              Text("Se sim, como consguiu indetificar que é phishing?", style: TextStyle(fontSize: 20),),
+               SizedBox(
+                height: 12,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: TextFormField(
+                  controller: _controllerMotivo,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    // hintText: '',
+                    labelText: 'Justificativa caso sim',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+                Text("Se não, como consguiu indetificar que não é phishing?", style: TextStyle(fontSize: 20),),
+               SizedBox(
+                height: 12,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  // hintText:
+                  //     'se não, como consguiu indetificar que não é phishing?',
+                  labelText: 'Justificativa caso não',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+              Container(
+                  padding: const EdgeInsets.only(left: 150.0, top: 40.0),
+                  child: ElevatedButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.red,
+                      foregroundColor: Color.fromARGB(255, 7, 7, 7),
+                      padding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
+                    ),
+                    child: const Text('Responder'),
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Processing Data')),
+                        );
+                      }
+                    },
+                  )),
+            ],
+          ),
         ),
       ),
     );
@@ -153,5 +191,86 @@ class _FormularioDeRespostaState extends State<FormularioDeResposta> {
 }
 
 //se é phishing
-//como consguiu indetificar que é phishing
-//pq não é phishing
+//se sim, como consguiu indetificar que é phishing?
+//se não, como consguiu indetificar que não é phishing?
+
+// Padding(
+//         padding: const EdgeInsets.all(16.0),
+//         child: Column(
+//           children: [
+//             Column(
+//               children: [
+//                 Padding(
+//                   padding: EdgeInsets.only(bottom: 8),
+//                   child: TextField(
+//                     controller: _controllerSimOuNao,
+//                     autofocus: true,
+//                     keyboardType: TextInputType.text,
+//                     style: TextStyle(fontSize: 17),
+//                     decoration: InputDecoration(
+//                         contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+//                         hintText: "O exemplo mostrado é Phishing? (sim ou não)",
+//                         filled: true,
+//                         fillColor: Colors.white,
+//                         border: OutlineInputBorder(
+//                             borderRadius: BorderRadius.circular(32))),
+
+//                   ),
+//                 ),
+//                 Padding(
+//                   padding: EdgeInsets.only(bottom: 8),
+//                   child: TextField(
+//                     controller: _controllerMotivo,
+//                     autofocus: true,
+//                     keyboardType: TextInputType.text,
+//                     style: TextStyle(fontSize: 17),
+//                     decoration: InputDecoration(
+//                         contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+//                         hintText:
+//                             "Como consguiu indetificar que o exemplo mostrado é phishing?",
+//                         filled: true,
+//                         fillColor: Colors.white,
+//                         border: OutlineInputBorder(
+//                             borderRadius: BorderRadius.circular(32))),
+//                   ),
+//                 ),
+//                 Padding(
+//                   padding: EdgeInsets.only(bottom: 8),
+//                   child: TextField(
+//                     controller: _controllerMotivo,
+//                     autofocus: true,
+//                     keyboardType: TextInputType.text,
+//                     style: TextStyle(fontSize: 17),
+//                     decoration: InputDecoration(
+//                         contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+//                         hintText:
+//                             "Caso não seja um phishing, nós diga o porque.",
+//                         filled: true,
+//                         fillColor: Colors.white,
+//                         border: OutlineInputBorder(
+//                             borderRadius: BorderRadius.circular(32))),
+//                   ),
+//                 ),
+//                 Padding(
+//                     padding: EdgeInsets.only(top: 16, bottom: 10),
+//                     child: TextButton(
+//                       style: TextButton.styleFrom(
+//                           foregroundColor: Colors.blue,
+//                           padding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+//                           shape: RoundedRectangleBorder(
+//                               borderRadius: BorderRadius.circular(32))),
+//                       child: Text(
+//                         "Responder",
+//                         style: TextStyle(
+//                             color: Color.fromARGB(255, 51, 5, 115),
+//                             fontSize: 20),
+//                       ),
+//                       onPressed: (() {
+//                         _salvandoRespostasDoUsuario();
+//                       }),
+//                     )),
+//               ],
+//             )
+//           ],
+//         ),
+//       ),
