7ffeb9932a59397ec8e86187d54505d143faebaf
Logan Cardoso <102470246+Logan-cic@users.noreply.github.com>
2023-02-21 13:46:57 -0300

    removendo image

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.indigo,
+
         brightness: Brightness.dark,
       ),
       home: Screen2(),
