04f2bc6d40c495b9f6c79fb847d7b0a5c32f8d05
Logan-cic <logan.cc@outlook.com>
2023-03-26 16:49:39 -0300

    caminho

--- lib/Screens/SMS/SmsScreen3.dart
+++ lib/Screens/SMS/SmsScreen3.dart
@@ -1,52 +1,49 @@
 import 'package:flutter/material.dart';
+import '../../Widgets/sms_body/smsBody.dart';
 import '../../Widgets/sms_body/smsSender.dart';
+import '../../Widgets/sms_header/smsHeader.dart';
 import '../../page/perguntas.dart';
-import '../../widgets/sms_body/smsBody.dart';
-import '../../widgets/sms_header/smsHeader.dart';
-
 
 class SmsScreen3 extends StatelessWidget {
-
-
-  const SmsScreen3({Key? key}): super(key: key);
-
+  const SmsScreen3({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-          floatingActionButton: FloatingActionButton(
-            child: const Icon(Icons.add),
-            onPressed: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: ((context) => Perguntas())));
-            },
-          ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: ((context) => Perguntas())));
+          },
+        ),
         backgroundColor: Colors.black,
-            appBar: AppBar(
-              backgroundColor: Colors.black,
-              elevation: 0,
-              title: const Text(
-                "Game Phishing",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-            ),
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          elevation: 0,
+          title: const Text(
+            "Game Phishing",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+        ),
         body: SingleChildScrollView(
           child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 12),  
+            padding: EdgeInsets.symmetric(horizontal: 12),
             child: Column(
               children: [
                 Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
                   child: Stack(
-                    children: [ 
-              SmsHeader(contato: 'SKY Brasil',),
-              chatSms(),
-              SmsBackGround(),
-              SmsSender(),
+                    children: [
+                      SmsHeader(
+                        contato: 'SKY Brasil',
+                      ),
+                      chatSms(),
+                      SmsBackGround(),
+                      SmsSender(),
                     ],
                   ),
                 ),
@@ -55,45 +52,56 @@ class SmsScreen3 extends StatelessWidget {
           ),
         ),
       ),
-    );  
+    );
   }
 }
 
-class ChatMessege{
+class ChatMessege {
   String messageContent;
   String messageType;
   ChatMessege({required this.messageContent, required this.messageType});
 }
-  Widget chatSms() {
-    return Stack(
-          alignment: Alignment.topCenter,
-          children: <Widget>[
-            ListView.builder(
-              itemCount: messages.length,
-              shrinkWrap: true,
-              padding: EdgeInsets.only(top: 70,bottom: 50),
-              physics: NeverScrollableScrollPhysics(),
-              itemBuilder: (context, index){
-                return Container(
-                  padding: EdgeInsets.only(left: 5,right: 5,top: 2,bottom: 10),
-                  child: Align(
-                    alignment: (messages[index].messageType == "receiver"?Alignment.topLeft:Alignment.topRight),
-                    child: Container(
-                      decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: (messages[index].messageType  == "receiver"?Colors.grey.shade200:Colors.lightBlue[700]),
-                    ),
-                    padding: EdgeInsets.symmetric(vertical: 12, horizontal: 12),
-                    child: Text(messages[index].messageContent, style: TextStyle(fontSize: 15),),
-                    ),
-                  ),  
-                );
-              },
+
+Widget chatSms() {
+  return Stack(
+    alignment: Alignment.topCenter,
+    children: <Widget>[
+      ListView.builder(
+        itemCount: messages.length,
+        shrinkWrap: true,
+        padding: EdgeInsets.only(top: 70, bottom: 50),
+        physics: NeverScrollableScrollPhysics(),
+        itemBuilder: (context, index) {
+          return Container(
+            padding: EdgeInsets.only(left: 5, right: 5, top: 2, bottom: 10),
+            child: Align(
+              alignment: (messages[index].messageType == "receiver"
+                  ? Alignment.topLeft
+                  : Alignment.topRight),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: (messages[index].messageType == "receiver"
+                      ? Colors.grey.shade200
+                      : Colors.lightBlue[700]),
+                ),
+                padding: EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+                child: Text(
+                  messages[index].messageContent,
+                  style: TextStyle(fontSize: 15),
+                ),
+              ),
             ),
-          ],  
-        );
+          );
+        },
+      ),
+    ],
+  );
 }
 
 List<ChatMessege> messages = [
-    ChatMessege(messageContent: "Assine SKY HDTV: Telecine + Series + Futebol + Desenhos APENAS 89,95 C/ 2 Pontos e Inst. Gratis! Solicite Agora no Link do WhatsApp:. wa.me/556198153931", messageType: "receiver"),
+  ChatMessege(
+      messageContent:
+          "Assine SKY HDTV: Telecine + Series + Futebol + Desenhos APENAS 89,95 C/ 2 Pontos e Inst. Gratis! Solicite Agora no Link do WhatsApp:. wa.me/556198153931",
+      messageType: "receiver"),
 ];
